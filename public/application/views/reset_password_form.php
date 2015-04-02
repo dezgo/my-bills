30a66cb48081d2efdd094bf35364377bd0fc2de9
Derek Gillett <github@derekgillett.com>
2015-04-02 18:12:51 +1100

    update tests start playing with securimage for captcha on initate password reset page

--- public/application/views/reset_password_form.php
+++ public/application/views/reset_password_form.php
@@ -7,7 +7,19 @@
 	echo form_label('Email address','lbl_email_address');
 	echo form_input('email_address', '', 'size=30 placeholder="Email address" autofocus');
 
-	echo form_submit('submit', 'Reset');
+	// show captcha HTML using Securimage::getCaptchaHtml()
+	require_once APPPATH.'third_party/securimage.php';
+	$options = array();
+	$options['input_name'] = 'ct_captcha'; // change name of input element for form post
+
+	if (!empty($_SESSION['ctform']['captcha_error'])) {
+		// error html to show in captcha output
+		$options['error_html'] = $_SESSION['ctform']['captcha_error'];
+	}
+
+    echo Securimage::getCaptchaHtml($options);
+    
+    echo form_submit('submit', 'Reset');
 	echo form_close();
 
 	echo '<br>Don\'t have an account?&nbsp;';
